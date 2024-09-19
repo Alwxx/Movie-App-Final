06e25aa6bf613ec84d6683b6f904a5f33b89b65c
Alwin <alwinsunnypers@gmail.com>
2024-09-19 22:57:32 +0530

    last commit

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: 'https://movie-app-final-serverside.vercel.app/',
     headers: {
         'Authorization': 'Bearer YOUR_GITHUB_TOKEN',
         'Content-Type': 'application/json',
