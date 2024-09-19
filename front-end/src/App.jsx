5ef081c5783becea9f6528d349d00c7f3e93f0f3
Alwin <alwinsunnypers@gmail.com>
2024-09-19 19:00:41 +0530

    later commit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MovieList from "./components/MovieList/MovieList";
-import MovieCard from "./components/moviecard/Moviecard";
 import "../src/App.css";
 import { ThemeProvider } from "./context/ThemeContext";
-import Navbar from "./components/navbar/Navbar";
 import AppRouter from "./Router.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MoviePage from "./pages/MoviePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import ProfilePage from "./pages/ProfilePage";
 import Navbar from "./components/navbar/Navbar";
 import MovieList from "./components/MovieList/MovieList";
-import MovieCard from "./components/moviecard/MovieCard";
+import MovieCard from "./components/moviecard/Moviecard";
 
 function AppRouter() {
   return (
@@ -17,7 +16,7 @@ function AppRouter() {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/" element={<MovieList />} />
-        <Route path="/movie/:id" element={<MovieCard />} />
+        <Route path="/movie/:id" element={MovieCard} />
       </Routes>
     </Router>
   );
