5d63657356624789b0c225bf10d6e85be41e1906
hamid_tahir <hamidtahir2000@gmail.com>
2024-10-03 17:11:31 +0500

    Vercel Config

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import ProfilePage from "./pages/ProfilePage";
 import Navbar from "./components/navbar/Navbar";
-import MovieList from "./components/MovieList/MovieList";
 import MovieCard from "./components/moviecard/MovieCard";
 import HomePage from "./pages/HomePage";
 import Favorites from "./pages/Favorites";
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,24 +4,9 @@ import api from "../services/api";
 import MoviesHero from "../components/MoviesHero";
 
 const HomePage = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await api.get("/movies");
-        setMovies(response.data);
-      } catch (error) {
-        console.error("Error fetching movies:", error);
-      }
-    };
-    fetchMovies();
-  }, []);
-
   return (
     <div>
       <MoviesHero />
-      
     </div>
   );
 };
